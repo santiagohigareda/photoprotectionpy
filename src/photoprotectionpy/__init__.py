c3723b91e948d96fc637d5d2cbca3ccea2e07ad7
Santiago Guerrero-Higareda <a01636858@tec.mx>
2025-04-01 12:36:11 -0600

    Update __init__.py

--- src/photoprotectionpy/__init__.py
+++ src/photoprotectionpy/__init__.py
@@ -3,3 +3,4 @@ from .adjusted_spf import adjspf
 from .uva_protectionfactor import uvapf
 from .uva_exposuredose import uvadose
 from .critical_wavelength import criticalwave
+__all__ = ['ispf', 'adjspf', 'uvapf', 'uvadose', 'criticalwave']
